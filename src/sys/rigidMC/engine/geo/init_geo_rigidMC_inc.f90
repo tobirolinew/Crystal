deb337ad9a1d76f83a9b763318251cf1ac628603
Tarunroy7495 <tarunroy.125@gmail.com>
2026-09-16 13:33:19 -0400

    Change polish option to true in optimize call

--- src/sys/rigidMC/engine/geo/init_geo_rigidMC_inc.f90
+++ src/sys/rigidMC/engine/geo/init_geo_rigidMC_inc.f90
@@ -69,7 +69,7 @@
                         rad_relax=lview%opt.eq.-1,&
                         dump=lview%dump,&
                         npad=npad,&
-                        polish=max_order.eq.0)
+                        polish=.true.)
             case default
                call catch_error(&
                         err=.true.,&
